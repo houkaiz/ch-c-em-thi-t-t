4ae29c4e2f0c2740c6b84d77cd0937c1252895b8
houkaiz <86913754+houkaiz@users.noreply.github.com>
2021-07-06 20:34:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Bii ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `12 năm của một đời người ngắn lắm
+    ngày mai và ngày kia là ngày em thi 
+    Đằng sau chữ yêu đây là những lời cổ động chúc em thi tốt`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    title: 'Đại Học Sư Phạm Hà Nội',
+    desc: 'làm vợ Phong :3 ',
+    btnYes: 'thi đỗ <33',
+    btnNo: 'thi trượt :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'chúc em thi đỗ NV1 <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'mai nhớ mang các kiến thức và một tâm hôn dũng cảm.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://m.me/ Trần Ngọc Phong '  //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
